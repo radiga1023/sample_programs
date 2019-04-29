7392454c685e6833b6631dd161baf0bea86d6d44
radiga1023 <r.adiga1023@gmail.com>
2019-04-29 09:45:40 +0530

    Update PrintPrimeNumber.cpp

--- PrintPrimeNumber.cpp
+++ PrintPrimeNumber.cpp
@@ -6,7 +6,7 @@ void seiveOfErathostenesis(int n, bool* prime)
     // Create a boolean array "prime[0..n]" and initialize 
     // all entries it as true. A value in prime[i] will 
     // finally be false if i is Not a prime, else true. 
-    for (int p = 2; p*p <= n; p++)
+    for (int p = 2; p < n; p++)
     {
         // If prime[p] is not changed, then it is a prime
         if (prime[p] == true)
@@ -15,14 +15,14 @@ void seiveOfErathostenesis(int n, bool* prime)
             // equal to the square of it 
             // numbers which are multiple of p and are 
             // less than p^2 are already been marked. 
-            for (int i = p*p; i < n; i += p)
+            for (int i = p*2; i < n; i += p)
             {
                 prime[i] = false;
             }
         }
     }
     //Print all the Prime Numebers
-    for (int p = 2; p <= n; p++)
+    for (int p = 2; p < n-2; p++)
     {
         if (prime[p] == true)
             cout << p << " ";
