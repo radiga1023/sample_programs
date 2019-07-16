191a3b5359cfd93cf238315dec1a821be74f9c3e
radiga1023 <r.adiga1023@gmail.com>
2019-07-16 11:28:29 +0530

    Update InvertBinaryTree.cpp

--- tree/InvertBinaryTree.cpp
+++ tree/InvertBinaryTree.cpp
@@ -23,8 +23,8 @@ class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
         if (root == NULL) return NULL;
-        TreeNode* right = invertTree(root->left);
-        TreeNode* left = invertTree(root->right);
+        TreeNode* right = invertTree(root->right);
+        TreeNode* left = invertTree(root->left);
         root->left = right;
         root->right = left;
         return root;
