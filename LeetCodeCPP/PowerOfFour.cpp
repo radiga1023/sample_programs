ef0d9d3a76fc7665b71108f1199ab592f6fa9ade
radiga1023 <r.adiga1023@gmail.com>
2019-07-29 15:08:07 +0530

    Create PowerOfFour.cpp

---
+++ LeetCodeCPP/PowerOfFour.cpp
@@ -0,0 +1,10 @@
+
+
+class Solution {
+public:
+    bool isPowerOfFour(int num) {
+        if ((num > 0) && ((num & num -1) == 0) && ((num & 0x55555555) != 0))
+            return true;
+        else
+            return false;
+    };
