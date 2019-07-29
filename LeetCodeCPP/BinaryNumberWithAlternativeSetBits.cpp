4260d5c9e2499c1e72481b451273691008037dae
radiga1023 <r.adiga1023@gmail.com>
2019-07-29 19:40:30 +0530

    Create BinaryNumberWithAlternativeSetBits.cpp

---
+++ LeetCodeCPP/BinaryNumberWithAlternativeSetBits.cpp
@@ -0,0 +1,11 @@
+
+
+/*
+https://leetcode.com/problems/binary-number-with-alternating-bits
+*/
+class Solution {
+public:
+    bool hasAlternatingBits(int n) {
+        return (((n & n >> 1) == 0) && ((n & n>> 2) == (n >> 2)));
+    }
+};
